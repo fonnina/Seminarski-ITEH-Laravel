68a8bd1344ae82d15d0916e68fdf754b88d81b59
fonnina <andrijasevicnina52@gmail.com>
2023-02-16 15:28:05 +0100

    APIAuthController i funkcija za prijavu korisnika

---
+++ app/Http/Controllers/APIAuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+
+class APIAuthController extends Controller
+{
+    public function prijava(Request $request)
+    {
+
+        $user = User::where('korisnicko_ime', $request->get('korisnicko_ime'))->first();
+
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Greška! Pokušajte ponovo',
+            ]);
+        } else {
+            if ($user->lozinka == $request->get('lozinka'))
+                return response()->json([
+                    'message' => 'Uspešno ste se prijavili',
+                ]);
+            else
+                return response()->json([
+                    'message' => 'Greška! Pokušajte ponovo',
+                ]);
+        }
+    }
+}
